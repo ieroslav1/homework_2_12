2ed500f480af36a8a44da6f2829048408cefd2ad
ieroslav1 <yaraboom@rambler.ru>
2023-05-19 23:33:47 +0300

    Код задания 2.3 Калькулятор

---
+++ src/main/java/pro/sky/homework_2_12/CalcService.java
@@ -0,0 +1,15 @@
+package pro.sky.homework_2_12;
+
+public interface CalcService {
+
+    String welcome();
+
+    int plusResults(int num1, int num2);
+
+    int minusResults(int num1, int num2);
+
+    int multiplyResults(int num1, int num2);
+
+    int divideResults(int num1, int num2);
+
+}
