6a3200b013c9929877d92eff2d4c10042caca1ef
ieroslav1 <yaraboom@rambler.ru>
2023-05-19 23:22:10 +0300

    1 commit

---
+++ src/main/java/pro/sky/homework_2_12/Homework212Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_2_12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework212Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework212Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/homework_2_12/Homework212ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_2_12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework212ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
